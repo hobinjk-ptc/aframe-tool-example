cb3af5a3de78d4115dc7ca4d4adfac9adee09287
James Hobin <jhobin@ptc.com>
2020-07-15 15:42:05 -0400

    Refactor into scene component

--- threejsTestTool/main.js
+++ threejsTestTool/main.js
@@ -1,71 +1,65 @@
-let aframeCamera = document.getElementById('camera');
-let aframeContainer = document.getElementById('container');
+AFRAME.registerComponent('spatial-tool', {
+  init: function() {
+    this.isProjectionMatrixSet = false;
 
-let camera, container, renderer;
-let rendererWidth = screen.height; // width is height because landscape orientation
-let rendererHeight = screen.width; // height is width
-let isProjectionMatrixSet = false;
+    this.aframeCamera = this.el.querySelector('a-camera');
+    this.aframeContainer = this.el.querySelector('#container');
 
-let spatialInterface;
+    console.log('acam', this.aframeCamera);
+    console.log('acon', this.aframeContainer);
 
-if (typeof SpatialInterface !== 'undefined') {
-  spatialInterface = new SpatialInterface();
-}
-
-window.onload = function() {
-  init();
-};
-
-function init() {
-  // 7. Use the Spatial Toolbox APIs to prepare to render correctly
-  if (spatialInterface) {
-    spatialInterface.setFullScreenOn();
-    spatialInterface.addMatrixListener(renderScene);
-  }
-}
-
-// 8. This main rendering loop will get called 60 times per second
-function renderScene(modelViewMatrix, projectionMatrix) {
-  camera = aframeCamera.object3D.children[0];
-  container = aframeContainer.object3D;
-  renderer = document.querySelector('a-scene').renderer;
-
-  // 9. only set the projection matrix for the camera 1 time, since it stays the same
-  if (projectionMatrix.length > 0) {
-    setMatrixFromArray(camera.projectionMatrix, projectionMatrix);
-    isProjectionMatrixSet = true;
-  }
-
-  // 10. Every frame, set the position of the container to the modelViewMatrix
-  if (isProjectionMatrixSet) {
-    if (container.matrixAutoUpdate) {
-      container.matrixAutoUpdate = false;
+    let spatialInterface;
+    if (typeof SpatialInterface !== 'undefined') {
+      spatialInterface = new SpatialInterface();
     }
-    setMatrixFromArray(container.matrix, modelViewMatrix);
-  }
-}
 
-// 11. This is just a helper function to set a three.js matrix using an array
-function setMatrixFromArray(matrix, array) {
-  matrix.set(
-    array[0], array[4], array[8], array[12],
-    array[1], array[5], array[9], array[13],
-    array[2], array[6], array[10], array[14],
-    array[3], array[7], array[11], array[15]);
-}
+    if (spatialInterface) {
+      spatialInterface.setFullScreenOn();
+      spatialInterface.addMatrixListener(this.renderScene.bind(this));
+    }
 
-// 12. Make sure the 3d renderer always has the right aspect ratio of the screen
-window.addEventListener('resize', function() {
-  rendererWidth = window.innerWidth;
-  rendererHeight = window.innerHeight;
+    window.addEventListener('resize', this.onResize.bind(this));
+  },
 
-  if (camera) {
-    camera.aspect = rendererWidth / rendererHeight;
-    camera.updateProjectionMatrix();
-  }
+  renderScene: function(modelViewMatrix, projectionMatrix) {
+    this.camera = this.aframeCamera.object3D.children[0];
+    this.container = this.aframeContainer.object3D;
+    this.renderer = this.el.renderer;
 
-  if (renderer) {
-    renderer.setSize(rendererWidth, rendererHeight);
-  }
+    if (projectionMatrix.length > 0) {
+      this.setMatrixFromArray(this.camera.projectionMatrix, projectionMatrix);
+      this.isProjectionMatrixSet = true;
+    }
+
+    // 10. Every frame, set the position of the container to the modelViewMatrix
+    if (this.isProjectionMatrixSet) {
+      if (this.container.matrixAutoUpdate) {
+        this.container.matrixAutoUpdate = false;
+      }
+      this.setMatrixFromArray(this.container.matrix, modelViewMatrix);
+    }
+  },
+
+  onResize: function() {
+    let rendererWidth = window.innerWidth;
+    let rendererHeight = window.innerHeight;
+
+    if (this.camera) {
+      this.camera.aspect = rendererWidth / rendererHeight;
+      this.camera.updateProjectionMatrix();
+    }
+
+    if (this.renderer) {
+      this.renderer.setSize(rendererWidth, rendererHeight);
+    }
+  },
+
+  // This is just a helper function to set a three.js matrix using an array
+  setMatrixFromArray: function(matrix, array) {
+    matrix.set(
+      array[0], array[4], array[8], array[12],
+      array[1], array[5], array[9], array[13],
+      array[2], array[6], array[10], array[14],
+      array[3], array[7], array[11], array[15]);
+  },
 });
-
